9eb99e0842ed714acacce1fc46aacb7d3a7dc101
Denys14Samoilenko <d.samoilenko92@gmail.com>
2023-10-24 16:27:25 +0300

    add banner-slider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.scss';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
+import { Slider } from './components/Slider/Slider';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Slider />
       <Footer />
     </div>
   );
