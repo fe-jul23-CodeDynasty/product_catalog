58b385d36fb2b4220b163e8d735e8ab7015bab07
vanom <envinenvin@gmail.com>
2023-10-24 16:44:05 +0300

    fixed conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import './App.scss';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
-import { BurgerMenu } from './components/BurgerMenu/BurgerMenu';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <BurgerMenu />
       <Footer />
     </div>
   );
