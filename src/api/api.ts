9416e4b39316a452e7282d219194715c3cd11294
Denys14Samoilenko <d.samoilenko92@gmail.com>
2023-10-26 12:36:34 +0300

    feat:add api and load phones from api

---
+++ src/api/api.ts
@@ -0,0 +1,6 @@
+const API_URL = 'https://product-catalog-be-qps4.onrender.com/products';
+
+export function getPhones() {
+  return fetch(API_URL)
+    .then(response => response.json());
+}
--- src/components/Phones/Phones.tsx
+++ src/components/Phones/Phones.tsx
@@ -1,3 +1,19 @@
-export const Phones = () => {
+/* eslint-disable no-console */
+import React, { useState, useEffect } from 'react';
+import { getPhones } from '../../api/api';
+
+export const Phones: React.FC = () => {
+  const [phones, setPhones] = useState([]);
+
+  useEffect(() => {
+    getPhones()
+      .then(setPhones)
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
+  console.log(phones);
+
   return <h1>Phones pages</h1>;
 };
