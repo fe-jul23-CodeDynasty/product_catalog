53fa5ea81317b6f71a55fadb8279d2ea6d8b3b45
Yevhenii Voronin <voronine@ukr.net>
2023-10-26 17:24:24 +0300

    add:pafegrid

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,3 +1,21 @@
+import { Slider } from '../Slider/Slider';
+import { ByCategories } from '../byCategoriesSection/ByCategories';
+import './home.scss';
+
 export const Home = () => {
-  return <h1>Home Page</h1>;
+  return (
+    <>
+      <div className="wallpapper">
+        <div className="container-home">
+          <h1 className="container-home__h1">Welcome to Nice Gadgets store!</h1>
+          <div className="container-home__slider">
+            <Slider />
+          </div>
+          <div className="container-home__by-categories">
+            <ByCategories />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
