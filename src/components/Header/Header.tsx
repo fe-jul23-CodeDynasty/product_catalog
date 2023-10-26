37977f90ddb8cfb7d7f698d5e784c8674c04c6ef
maksym-mishchanchuk <mishchanchuk.maksym@gmail.com>
2023-10-26 23:18:57 +0300

    add: adaptive styles for burger-menu, fidex all problem with adaptive in header component, create navigition component, delete dublicate code in burger menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,44 +1,21 @@
 import './Header.scss';
-
 import { NavLink } from 'react-router-dom';
-import logo_main from '../../images/logo_main.svg';
 import burger_menu from '../../images/burger_menu.svg';
 import favourites_heart_like from '../../images/favourites_heart_like.svg';
 import shopping_bag from '../../images/shopping_bag.svg';
+import { Navigation } from '../Navigation/Navigation';
+import logo_main from '../../images/logo_main.svg';
 
 export const Header = () => {
   return (
     <header className="header">
       <div className="header__content">
-        <div className="nav_container">
+        <div className="header-nav-container">
           <NavLink to="/" className="logo">
             <img className="logo__img" src={logo_main} alt="Logo link" />
           </NavLink>
 
-          <nav className="nav">
-            <ul className="nav__list">
-              <li className="nav__item">
-                <NavLink to="/" className="nav__item__link">
-                  Home
-                </NavLink>
-              </li>
-              <li className="nav__item">
-                <NavLink to="/phones" className="nav__item__link">
-                  phones
-                </NavLink>
-              </li>
-              <li className="nav__item">
-                <NavLink to="/tablets" className="nav__item__link">
-                  tablets
-                </NavLink>
-              </li>
-              <li className="nav__item" data-qa="hover">
-                <NavLink to="/accessories" className="nav__item__link">
-                  accessories
-                </NavLink>
-              </li>
-            </ul>
-          </nav>
+          <Navigation />
         </div>
 
         <div className="container__heart-like-shopping-bag">
@@ -50,7 +27,10 @@ export const Header = () => {
           </a>
         </div>
 
-        <a className="burger__menu" href="/">
+        <a
+          className="burger__menu"
+          href="/"
+        >
           <img src={burger_menu} alt="menu" />
         </a>
       </div>
---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,33 @@
+import { NavLink } from 'react-router-dom';
+import './Navigation.scss';
+
+export const Navigation = () => {
+  return (
+    <div className="nav_container">
+      <nav className="nav">
+        <ul className="nav__list">
+          <li className="nav__item">
+            <NavLink to="/" className="nav__item__link">
+              Home
+            </NavLink>
+          </li>
+          <li className="nav__item">
+            <NavLink to="/phones" className="nav__item__link">
+              phones
+            </NavLink>
+          </li>
+          <li className="nav__item">
+            <NavLink to="/tablets" className="nav__item__link">
+              tablets
+            </NavLink>
+          </li>
+          <li className="nav__item" data-qa="hover">
+            <NavLink to="/accessories" className="nav__item__link">
+              accessories
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
